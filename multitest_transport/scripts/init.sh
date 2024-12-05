3e23b634826fedc47e4a114bfe5958e4250c173a
Google Team <no-reply@google.com>
2024-12-05 01:49:24 -0800

    No public description
    PiperOrigin-RevId: 703027581
    Change-Id: I42dd863e9f515364a49feee3b3f79d3fdb8139df

--- multitest_transport/scripts/init.sh
+++ multitest_transport/scripts/init.sh
@@ -312,6 +312,7 @@ else
     --enable_device_system_settings_change=false \
     --enable_external_master_server=true \
     --enable_root_device=false \
+    --enable_test_log_collector=true \
     --master_grpc_target="${OLC_SERVER_GRPC_TARGET}" \
     --mute_android=false \
     --public_dir="${MTT_LOG_DIR}" \
