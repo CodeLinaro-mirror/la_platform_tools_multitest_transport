6ff5669437c0812223eea976eb389ed624a6f922
Google Team <no-reply@google.com>
2025-05-12 20:24:15 -0700

    No public description
    PiperOrigin-RevId: 758022163
    Change-Id: If553820ad43735d289cdde91ae2514ff703c0bd5

--- multitest_transport/integration_test/ats2_e2e_test.py
+++ multitest_transport/integration_test/ats2_e2e_test.py
@@ -37,7 +37,7 @@ _ARTIFACTS_DOWNLOAD_URL = (
     'https://www.googleapis.com/android/internal/build/'
     'v3/builds/%s/%s/attempts/latest/artifacts/%s/url'
 )
-_BUILD_ID = '11807604'
+_BUILD_ID = '13281750'
 _BUILD_TARGET = 'aosp_cf_x86_64_phone-trunk_staging-userdebug'
 _CVD_HOST_PACKAGE_URL = _ARTIFACTS_DOWNLOAD_URL % (
     _BUILD_ID,
