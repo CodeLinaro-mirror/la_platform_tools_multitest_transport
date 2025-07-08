6b631100adb4ca0de6f531dae575828d47b658d2
Jianxin Gao <jianxingao@google.com>
2025-07-08 03:19:07 -0700

    No public description
    PiperOrigin-RevId: 780489763
    Change-Id: Ief02e2dcef228cb006633bcb70c0a731767da1c6

--- multitest_transport/cli/_build.sh
+++ multitest_transport/cli/_build.sh
@@ -100,11 +100,10 @@ RUN export DEBIAN_FRONTEND=noninteractive; apt update -qq; apt install -y -qq \
 RUN update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.10 1000
 
 COPY ./requirements.txt /tmp
-COPY ./constraints.txt /tmp
 RUN python3 -m ensurepip --upgrade
 RUN pip3 install --upgrade setuptools pip
 RUN pip3 install pex
-RUN pip3 install -r /tmp/requirements.txt -c /tmp/constraints.txt
+RUN pip3 install -r /tmp/requirements.txt
 RUN pip3 install --upgrade keyrings.alt
 
 # Upgrade to the latest verified version of protoc which supports python3.13.
@@ -168,7 +167,6 @@ pex --python="python3.13" --python="python3.12" --python="python3.11" \
   --pip-version latest-compatible \
   -D src \
   -r requirements.txt \
-  --constraints constraints.txt \
   -m multitest_transport.cli.cli \
   -o mtt
 
@@ -185,7 +183,6 @@ pex --python="python3.13" --python="python3.12" --python="python3.11" \
   --pip-version latest-compatible \
   -D src \
   -r requirements.txt \
-  --constraints constraints.txt \
   -m multitest_transport.cli.lab_cli \
   -o mtt_lab
 EOF
