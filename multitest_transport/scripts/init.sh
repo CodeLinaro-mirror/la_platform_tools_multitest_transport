f93c727ac967051bc6ab8819ed516df29833a4bb
Jianxin Gao <jianxingao@google.com>
2025-10-14 20:31:41 -0700

    Set lab server `--enable_caching_reserved_device` flag to true for ATS UI.
    PiperOrigin-RevId: 819536727
    Change-Id: I1496c07ad66ca6e7c8f5fd4d5a7d93e42a934349

--- multitest_transport/scripts/init.sh
+++ multitest_transport/scripts/init.sh
@@ -357,6 +357,7 @@ else
     --disable_wifi_util_func=true \
     --enable_android_device_ready_check=false \
     --enable_ats_file_server_uploader=true \
+    --enable_caching_reserved_device=true \
     --enable_device_state_change_recover=false \
     --enable_device_system_settings_change=false \
     --enable_external_master_server=true \
