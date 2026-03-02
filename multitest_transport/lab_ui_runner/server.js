73d7b27f94f6236c9157c0aa90a08f341c02d5a4
Google Team <no-reply@google.com>
2026-03-02 02:36:22 -0800

    No public description
    PiperOrigin-RevId: 877295404
    Change-Id: I27d09380912310e37a8ae2889aa887f9b8c382ca

--- multitest_transport/lab_ui_runner/server.js
+++ multitest_transport/lab_ui_runner/server.js
@@ -3,14 +3,19 @@ const path = require('path');
 const fs = require('fs');
 const app = express();
 const port = process.env.LAB_CONSOLE_PORT || 4200;
+const isDevMode = process.env.MTT_CLI_VERSION === 'dev';
+
+console.info('MTT_CLI_VERSION: ', process.env.MTT_CLI_VERSION);
 
 const indexHtmlPath = path.join(__dirname, 'public', 'index.html');
 let indexHtml = '';
-try {
-  indexHtml = fs.readFileSync(indexHtmlPath, 'utf8');
-} catch (err) {
-  console.error('Could not read index.html!', err);
-  process.exit(1);
+if (!isDevMode) {
+  try {
+    indexHtml = fs.readFileSync(indexHtmlPath, 'utf8');
+  } catch (err) {
+    console.error('Could not read index.html!', err);
+    process.exit(1);
+  }
 }
 
 app.use(express.static(path.join(__dirname, 'public')));
@@ -21,6 +26,22 @@ app.get('/*', function(req, res) {
   // path to the url, so that it will be proxy to the ${restPort} by the
   // main server.
 
+  // for performance issue, we read the index.html file content ahead
+  // of time, and won't re-read it from the file system every time when a
+  // request comes in. But in dev mdoe, we should re-read it every time, as we
+  // mihgt need to update the index.html file content.
+
+  let content = indexHtml;
+  if (isDevMode) {
+    try {
+      content = fs.readFileSync(indexHtmlPath, 'utf8');
+    } catch (err) {
+      console.error('Could not read index.html in dev mode!', err);
+      res.status(500).send('Could not read index.html!');
+      return;
+    }
+  }
+
   // append the init data
   const script = `
 <script type="application/json" id="app-data">
@@ -29,7 +50,7 @@ app.get('/*', function(req, res) {
   }
 </script>
 `;
-  res.status(200).send(indexHtml + script);
+  res.status(200).send(content + script);
 });
 
 app.listen(port, () => {
